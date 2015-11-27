93368ec21897c4e97de7bfd3cebfc24a5ce421f6
Bartosz Cieśla <bartociesla@gmail.com>
2015-11-27 17:58:21 +0100

    Działający capture

---
+++ KamerkaWF/KamerkaWF/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace KamerkaWF
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CaptureBox = new System.Windows.Forms.PictureBox();
+            this.CamerasCB = new System.Windows.Forms.ComboBox();
+            this.CaptureButton = new System.Windows.Forms.Button();
+            this.ResolutionCB = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.CaptureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // CaptureBox
+            // 
+            this.CaptureBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.CaptureBox.BackColor = System.Drawing.SystemColors.Desktop;
+            this.CaptureBox.Location = new System.Drawing.Point(12, 12);
+            this.CaptureBox.Name = "CaptureBox";
+            this.CaptureBox.Size = new System.Drawing.Size(625, 180);
+            this.CaptureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.CaptureBox.TabIndex = 0;
+            this.CaptureBox.TabStop = false;
+            // 
+            // CamerasCB
+            // 
+            this.CamerasCB.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.CamerasCB.FormattingEnabled = true;
+            this.CamerasCB.Location = new System.Drawing.Point(12, 198);
+            this.CamerasCB.Name = "CamerasCB";
+            this.CamerasCB.Size = new System.Drawing.Size(625, 21);
+            this.CamerasCB.TabIndex = 1;
+            this.CamerasCB.SelectedIndexChanged += new System.EventHandler(this.CamerasCB_SelectedIndexChanged);
+            // 
+            // CaptureButton
+            // 
+            this.CaptureButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.CaptureButton.Location = new System.Drawing.Point(12, 252);
+            this.CaptureButton.Name = "CaptureButton";
+            this.CaptureButton.Size = new System.Drawing.Size(122, 23);
+            this.CaptureButton.TabIndex = 2;
+            this.CaptureButton.Text = "Start / Stop";
+            this.CaptureButton.UseVisualStyleBackColor = true;
+            this.CaptureButton.Click += new System.EventHandler(this.CaptureButton_Click);
+            // 
+            // ResolutionCB
+            // 
+            this.ResolutionCB.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ResolutionCB.FormattingEnabled = true;
+            this.ResolutionCB.Location = new System.Drawing.Point(13, 225);
+            this.ResolutionCB.Name = "ResolutionCB";
+            this.ResolutionCB.Size = new System.Drawing.Size(624, 21);
+            this.ResolutionCB.TabIndex = 3;
+            this.ResolutionCB.SelectedIndexChanged += new System.EventHandler(this.ResolutionCB_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(649, 294);
+            this.Controls.Add(this.ResolutionCB);
+            this.Controls.Add(this.CaptureButton);
+            this.Controls.Add(this.CamerasCB);
+            this.Controls.Add(this.CaptureBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.CaptureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox CaptureBox;
+        private System.Windows.Forms.ComboBox CamerasCB;
+        private System.Windows.Forms.Button CaptureButton;
+        private System.Windows.Forms.ComboBox ResolutionCB;
+    }
+}
+
---
+++ KamerkaWF/KamerkaWF/Form1.cs
@@ -0,0 +1,127 @@
+﻿using AForge.Video;
+using AForge.Video.DirectShow;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KamerkaWF
+{
+    public partial class Form1 : Form
+    {
+        private FilterInfoCollection videoDevices;
+        private VideoCaptureDevice videoSource;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            videoDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            foreach (FilterInfo fi in videoDevices)
+            {
+                CamerasCB.Items.Add(fi.Name);
+            }
+            videoSource = null;
+        }
+
+        private void CamerasCB_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (CamerasCB.SelectedIndex >= 0)
+            {
+                ResolutionCB.Items.Clear();
+                videoSource = new VideoCaptureDevice(videoDevices[CamerasCB.SelectedIndex].MonikerString);
+                foreach (VideoCapabilities vc in videoSource.VideoCapabilities)
+                {
+                    ResolutionCB.Items.Add("Resolution: " + vc.FrameSize + ", frame rate: " + vc.MaximumFrameRate);
+                }
+            }
+        }
+
+        private void CaptureButton_Click(object sender, EventArgs e)
+        {
+            if (videoSource != null)
+            {
+                if (videoSource.IsRunning)
+                {
+                    videoSource.SignalToStop();
+                    videoSource.WaitForStop();
+                    CaptureBox.Invoke(
+                        new Action(
+                            delegate
+                            {
+                                CaptureBox.Image = null;
+                                CaptureBox.Invalidate();
+                            }));
+                }
+                else
+                {
+
+                    videoSource.NewFrame += videoSource_NewFrame;
+                    videoSource.Start();
+                }
+            }
+        }
+
+        void videoSource_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            Bitmap image = (Bitmap)eventArgs.Frame.Clone();
+            CaptureBox.Invoke(
+                new Action(
+                    delegate
+                    {
+                        CaptureBox.Image = image;
+                    }));
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (videoSource.IsRunning)
+            {
+                videoSource.SignalToStop();
+
+                for (int i = 0; i < 30; i++)
+                {
+                    if (!videoSource.IsRunning)
+                        break;
+                    System.Threading.Thread.Sleep(100);
+                    MessageBox.Show(i + "");
+                }
+
+                if (videoSource.IsRunning)
+                {
+                    videoSource.Stop();
+                }
+
+                videoSource = null;
+            }
+        }
+
+        private void ResolutionCB_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+            videoSource.VideoResolution = videoSource.VideoCapabilities[ResolutionCB.SelectedIndex];
+            if (videoSource.IsRunning)
+            {
+                videoSource.SignalToStop();
+                videoSource.WaitForStop();
+                CaptureBox.Invoke(
+                    new Action(
+                        delegate
+                        {
+                            CaptureBox.Image = null;
+                            CaptureBox.Invalidate();
+                        }));
+                videoSource.NewFrame += videoSource_NewFrame;
+                videoSource.Start();
+            }
+        }
+
+    }
+}
